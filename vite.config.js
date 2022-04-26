fbe34ae5e634bc22f25ed22c44a49ef3c3636327
Szasz Tamas-Csaba <szaszdex@gmail.com>
2022-04-26 17:45:03 +0300

    Metadata updates.

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,9 @@ import path from 'path';
 import { defineConfig } from 'vite';
 
 export default defineConfig({
+  server: {
+    port: 8080,
+  },
   build: {
     lib: {
       entry: path.resolve(__dirname, 'src/lib/index.ts'),
